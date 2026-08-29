76b61322de07b87e4cb4cd672add7c9e7500954d
Iryna <sobkalova.irina.111@gmail.com>
2026-08-29 17:02:59 +0300

    Add monthly performance analysis for store sales
    Added a query to analyze month-by-month performance trends of each store, including previous month revenue and month-over-month growth percentage.

--- sql/04_store_analysis.sql
+++ sql/04_store_analysis.sql
@@ -30,3 +30,44 @@ SELECT
 FROM coffee_sales
 GROUP BY store_location
 ORDER BY revenue DESC
+
+--Purpose: analysis of the month-by-month performance trends of each store.
+-- QUERY:
+WITH monthly_store_sales AS (
+    SELECT
+        TO_CHAR(transaction_date, 'MM') AS month,
+        store_location,
+        SUM(revenue) AS revenue
+    FROM coffee_sales
+    GROUP BY 1, 2
+)
+
+SELECT
+    month,
+    store_location,
+    revenue,
+    LAG(revenue) OVER (
+        PARTITION BY store_location
+        ORDER BY month
+    ) AS previous_month_revenue,
+    ROUND(
+        100.0 * (
+            revenue - LAG(revenue) OVER (
+                PARTITION BY store_location
+                ORDER BY month
+            )
+        ) / NULLIF(
+            LAG(revenue) OVER (
+                PARTITION BY store_location
+                ORDER BY month
+            ), 0
+        ),
+        2
+    ) AS mom_growth_pct
+FROM monthly_store_sales
+ORDER BY month, store_location
+
+-- Store Hell's Kitchen has the highest revenue. That's because store Hell's Kitchen has the highest transaction volume.
+-- The "Lower Manhattan" store has the lowest revenue. Customers at the "Lower Manhattan" store spend more per transaction but have a lower transaction volume.
+-- The share of total revenue is distributed among the stores almost evenly, with a difference of less than 1%.
+-- All stores show a decline in revenue in February and growth from March to June. "Hell's Kitchen" leads in revenue throughout the entire period.
