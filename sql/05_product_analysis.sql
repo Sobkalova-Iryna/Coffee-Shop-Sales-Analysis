f6bd5c6d5be3474c3b6b2834f73546efaa26198f
Iryna <sobkalova.irina.111@gmail.com>
2026-08-29 17:37:30 +0300

    Add product analysis SQL module
    This SQL module identifies the top products by revenue and those that generate the first 80% of total revenue.

---
+++ sql/05_product_analysis.sql
@@ -0,0 +1,52 @@
+-- ============================================================================
+-- SQL Module: 05_product_analysis.sql
+-- ============================================================================
+
+-- Purpose: identifying top products by revenue
+-- QUERY:
+SELECT
+    product_detail,
+    SUM(revenue) AS revenue,
+	RANK() OVER (
+        ORDER BY SUM(revenue) DESC
+    ) AS revenue_rank,
+	SUM(transaction_qty) AS units_sold,
+		RANK() OVER (
+        ORDER BY SUM(transaction_qty) DESC
+    ) AS units_sold_rank
+FROM coffee_sales
+GROUP BY product_detail
+LIMIT 10
+
+-- Purpose: identifying the products that generate the first 80% of revenue
+-- QUERY:
+WITH products AS (
+    SELECT
+        product_detail,
+        SUM(revenue) AS revenue
+    FROM coffee_sales
+    GROUP BY product_detail
+),
+
+ranked AS (
+    SELECT
+        product_detail,
+        revenue,
+        SUM(revenue) OVER (
+            ORDER BY revenue DESC
+        ) AS cumulative_revenue
+    FROM products
+)
+
+SELECT
+    product_detail,
+    revenue,
+    ROUND(
+        100.0 * cumulative_revenue
+        / SUM(revenue) OVER (),
+        2
+    ) AS cumulative_revenue_pct
+FROM ranked
+ORDER BY revenue DESC
+
+--
